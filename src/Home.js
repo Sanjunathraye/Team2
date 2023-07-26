bd134f0d78beb23fc5cd43af6e752e299e266fc2
Ranjanaraye <ranjanachanbatty@gmail.com>
2023-07-26 12:13:06 +0530

    r commit

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <h1>Demo</h1>
+        <button className='btn btn-success'>Submit</button>
+    </div>
+  )
+}
+
+export default Home
