98421be1eb1f14509a36c428eed65b33eb57a89c
Sanjunathraye <sanjunathraye54@gmail.com>
2023-07-26 12:21:10 +0530

    sanju commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Product from './Product';
 
 function App() {
   return (
     <div className="App">
       <h1>hello</h1>
+      <Product />
     </div>
   );
 }
---
+++ src/Product.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Product = () => {
+  return (
+    <div>
+      <button className='btn btn-success'>Product</button>
+    </div>
+  )
+}
+
+export default Product
